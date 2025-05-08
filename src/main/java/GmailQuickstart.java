37ba1ded081df8777f97337f2be6e070f99c23f9
josdem <joseluis.delacruz@gmail.com>
2025-05-08 13:13:02 -0600

    Cleaning some code

--- src/main/java/GmailQuickstart.java
+++ src/main/java/GmailQuickstart.java
@@ -24,63 +24,34 @@ import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
-/* class to demonstrate use of Gmail list labels API */
 public class GmailQuickstart {
-  /**
-   * Application name.
-   */
   private static final String APPLICATION_NAME = "Gmail API Java Quickstart";
-  /**
-   * Global instance of the JSON factory.
-   */
   private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-  /**
-   * Directory to store authorization tokens for this application.
-   */
   private static final String TOKENS_DIRECTORY_PATH = "tokens";
-
-  /**
-   * Global instance of the scopes required by this quickstart.
-   * If modifying these scopes, delete your previously saved tokens/ folder.
-   */
   private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_SEND);
   private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
-  /**
-   * Creates an authorized Credential object.
-   *
-   * @param HTTP_TRANSPORT The network HTTP Transport.
-   * @return An authorized Credential object.
-   * @throws IOException If the credentials.json file cannot be found.
-   */
   private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
       throws IOException {
-    // Load client secrets.
     InputStream in = GmailQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-    if (in == null) {
-      throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-    }
-    GoogleClientSecrets clientSecrets =
+
+      assert in != null;
+      GoogleClientSecrets clientSecrets =
         GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-    // Build flow and trigger user authorization request.
     GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
         HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
         .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
         .setAccessType("offline")
         .build();
     VerificationCodeReceiver receiver = new JmailerVerificationCodeReceiverImpl.Builder().setPort(8888).build();
-    Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-    //returns an authorized Credential object.
-    return credential;
+    return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
   }
 
   public static void main(String... args) throws IOException, GeneralSecurityException, MessagingException {
-    // Build a new authorized API client service.
     final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
 
     Credential credentials = getCredentials(HTTP_TRANSPORT);
-    System.out.println("access token: " + credentials.getAccessToken());
     Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credentials)
         .setApplicationName(APPLICATION_NAME)
         .build();
